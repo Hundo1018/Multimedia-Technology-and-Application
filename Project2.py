d2c7009a817ba559a1615dc4a46be636f9740d74
pl30987 <55215480+pl30987@users.noreply.github.com>
2021-03-11 12:40:27 +0800

    circle灰灰的不見了

--- Project2.py
+++ Project2.py
@@ -9,7 +9,7 @@ manFrame = cv2.imread("man.jpg")
 temp ,nCircleFrame =cv2.threshold( circleFrame,200,255,cv2.THRESH_TOZERO)
 temp ,nManFrame = cv2.threshold(manFrame,200,255,cv2.THRESH_TOZERO)
 
-nCircleFrame = cv2.morphologyEx(nCircleFrame,cv2.MORPH_CLOSE,np.ones((5,5)),iterations = 4)
+nCircleFrame = cv2.morphologyEx(nCircleFrame,cv2.MORPH_CLOSE,np.ones((3,3)),iterations = 9)
 nManFrame2 = cv2.dilate(nManFrame,np.ones((3,3)),iterations = 1)
 nManFrame = nManFrame2-nManFrame 
 
@@ -17,3 +17,4 @@ nManFrame = nManFrame2-nManFrame
 cv2.imshow("circle",np.hstack([circleFrame,nCircleFrame]))
 cv2.imshow("man",np.hstack([manFrame, nManFrame]))
 cv2.waitKey(0)
+cv2.destroyAllWindows()
