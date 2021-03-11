380f930f38f7b44b23692fc95777616047281719
Hundo1018 <f74169@gmail.com>
2021-03-11 12:16:16 +0800

    完成Circle補洞與Man找邊緣，但Circle補的洞不太滿

---
+++ Project2.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+
+circleFrame = cv2.imread("circle.jpg")
+manFrame = cv2.imread("man.jpg")
+
+
+temp ,nCircleFrame =cv2.threshold( circleFrame,200,255,cv2.THRESH_TOZERO)
+temp ,nManFrame = cv2.threshold(manFrame,200,255,cv2.THRESH_TOZERO)
+
+nCircleFrame = cv2.morphologyEx(nCircleFrame,cv2.MORPH_CLOSE,np.ones((5,5)),iterations = 4)
+nManFrame2 = cv2.dilate(nManFrame,np.ones((3,3)),iterations = 1)
+nManFrame = nManFrame2-nManFrame 
+
+
+cv2.imshow("circle",np.hstack([circleFrame,nCircleFrame]))
+cv2.imshow("man",np.hstack([manFrame, nManFrame]))
+cv2.waitKey(0)
