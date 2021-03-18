5fa7f86b3459aaa5dab0480d9283b0f1f45c26d4
Hundo1018 <f74169@gmail.com>
2021-03-18 12:22:29 +0800

    在每個圓中畫出方框 但會有包含整個畫面的框(尚未知道原因)

--- Project3.py
+++ Project3.py
@@ -11,19 +11,26 @@ coinFrame = cv2.resize(coinFrame,(nW,nH),interpolation=cv2.INTER_AREA)
 
 #灰階
 gCoinFrame = cv2.cvtColor(coinFrame,cv2.COLOR_BGR2GRAY)
-#cv2.imshow("o",gCoinFrame)
 
 #濾波
-fCoinFrame = cv2.bilateralFilter(gCoinFrame,100,75,75)
-#fCoinFrame = cv2.bilateralFilter(gCoinFrame,100,60,10)
+fCoinFrame = cv2.bilateralFilter(gCoinFrame,100,60,10)
 
 #二值化
 temp ,bCoinFrame =cv2.threshold(fCoinFrame,75,255,cv2.THRESH_BINARY )
-cv2.imshow("",np.hstack([bCoinFrame]))
 
+#侵蝕
+eCoinFrame = cv2.erode(bCoinFrame,np.ones((5,5)),iterations = 3)
+
+#物件連通
+num_labels,labels,states,centroids = cv2.connectedComponentsWithStats(eCoinFrame,connectivity=8)
+
+#畫方框
+nCoinFrame = coinFrame
+for s in states:
+    cv2.rectangle(nCoinFrame,(s[0],s[1]),(s[2]+s[0],s[3]+s[1]),(0,0,127),1)
 
 #顯示
-#cv2.imshow("test",np.hstack([fCoinFrame]))
+cv2.imshow(str(i),np.hstack([nCoinFrame]))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
