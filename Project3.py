640e6ebe9619046b209a43a50fd044c7a935f17b
Zhan Zhuang <t108590042@ntut.edu.tw>
2021-03-25 11:57:24 +0800

    大致完成 參數還需要調整

--- Project3.py
+++ Project3.py
@@ -1,29 +1,42 @@
 import cv2
 import numpy as np
 
-#read
-floor = cv2.imread("floor.jpg")
+# 第二題
+def Quiz_2():
+    # read
+    floor = cv2.imread("floor.jpg")
 
-#轉灰階
-floor_p = cv2.cvtColor(floor, cv2.COLOR_RGB2GRAY)
+    # resize
+    floor = cv2.resize(floor, (562, 1000), interpolation=cv2.INTER_AREA)
 
-#高斯模糊
-floor_p = cv2.GaussianBlur(floor_p, (7, 7), 0)
+    # 轉灰階
+    floor_p = cv2.cvtColor(floor, cv2.COLOR_RGB2GRAY)
 
-#二值化
-useless, floor_BIN = cv2.threshold(floor_p, 100, 255, cv2.THRESH_BINARY)
+    # 高斯模糊
+    floor_p = cv2.GaussianBlur(floor_p, (5, 5), 0)
 
-#畫出線條
-floor_lines = cv2.HoughLinesP(floor_BIN, 1, np.pi/180, 200, 10, 5)
-for p in floor_lines:
-    p = p[0]
-    cv2.line(floor, (p[0],p[1]), (p[2],p[3]), color=(0,255,0), thickness=2)
+    # 二值化
+    useless, floor_BIN = cv2.threshold(floor_p, 120, 255, cv2.THRESH_BINARY_INV)
 
-#resize
-#floor = cv2.resize(floor, (562,1000), interpolation=cv2.INTER_AREA)
+    # CLOSE
+    floor_close = cv2.morphologyEx(floor_BIN, cv2.MORPH_CLOSE, np.ones((5, 5)), iterations=1)
 
-cv2.imwrite("floor_r.jpg", np.hstack([floor, floor_BIN]))
-#cv2.waitKey()
-#floor_lines = cv2.HoughLinesP(floor_resize_gray, 1, np.pi/180, 200, 10, 5)
+    # 畫出線條
+    floor_lines = cv2.HoughLinesP(floor_close, 10, 3*np.pi/180, 500, 50, 20)
+    for p in floor_lines:
+        p = p[0]
+        cv2.line(floor, (p[0], p[1]), (p[2], p[3]),
+                 color=(0, 0, 255), thickness=2)
 
+    cv2.imshow("q2_result", floor)
+    cv2.imwrite("floor_r.jpg", np.hstack([floor_BIN, floor_close]))
+    cv2.imwrite("floor_line.jpg", floor)
+    cv2.waitKey()
+    print("done")
 
+# main function
+def main():
+    Quiz_2()
+
+# main
+main()
