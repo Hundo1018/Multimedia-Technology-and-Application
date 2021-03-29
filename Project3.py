104af3f93ae6850efe66511e441477bad6d9cc8f
Hundo1018 <f74169@gmail.com>
2021-03-29 20:18:54 +0800

    硬幹法完成題目1

--- Project3.py
+++ Project3.py
@@ -1,11 +1,15 @@
 import cv2
 import numpy as np
-
+from math import * 
+import random
 
 #
 # 第一題
 #
 def food_1():
+    usingKMean = False
+
+
     #讀取
     coinFrame = cv2.imread("coin.jpg")
 
@@ -18,7 +22,7 @@ def food_1():
     gCoinFrame = cv2.cvtColor(coinFrame,cv2.COLOR_BGR2GRAY)
 
     #HSV
-    hsvgCoinFrame = cv2.cvtColor(coinFrame, cv2.COLOR_BGR2HSV)
+    hsvCoinFrame = cv2.cvtColor(coinFrame, cv2.COLOR_BGR2HSV)
 
     #濾波
     fCoinFrame = cv2.bilateralFilter(gCoinFrame,100,60,10)
@@ -34,61 +38,94 @@ def food_1():
 
     #畫方框
     nCoinFrame = coinFrame
-    for s in states[1:]:
-        #print(s)
-        cv2.rectangle(nCoinFrame,(s[0],s[1]),(s[2]+s[0],s[3]+s[1]),(0,0,127),1)
-
-    l = []#資料點
-    lSets = []#資料點對應群集
-    kPoints = []#中心點位置
+    
+    if (usingKMean):
+        l = []#資料點
+        lSets = []#資料點對應群集
+        kPoints = []#中心點位置
 
 
-    #k mean分群(嘗試)
-    #拆成list
-    for s in states[1:]:
-        l.append(s[4])
-        lSets.append(0)
+        #k mean分群(嘗試)
+        #拆成list
+        for s in states[1:]:
+            l.append([s[4] , hsvCoinFrame[s[1]][s[0]][0]])
+            lSets.append(0)
+
+
+        #有1元5元10元50元，分成4群
+        #分類的點K，先放前四個
+        for i in l[:4]:
+            kPoints.append([i[0],i[1]])
         
+        #主循環
+        for loop in range(100):
+            #每個點分給最近的
+            pCount = 0#點的index，0~len(l)
+            for p in l:
+                kCount = 0#群集的index，0~3
+                minDis = 999999#最小距離
+                for k in kPoints:
+                    er = sqrt((p[0]-k[0])**2 + (p[1]-k[1])**2)
+                    if er <= minDis:
+                        lSets[pCount] = kCount
+                        minDis = er
+                    '''
+                    if abs(p-k)<= minDis:#如果這個點對某個中心最近
+                        lSets[pCount] = kCount#將此點設為該群集
+                        minDis = abs(p-k)
+                    '''
+                    kCount+=1
+                pCount+=1
 
-    #有1元5元10元50元，分成4群
-    #分類的點K，先放前四個
-    for i in l[:4]:
-        kPoints.append(i)
+            #重新計算每個群的中心(kPoints)
+            #各群數量與total
+            kn = [0,0,0,0]
+            ktx = [0,0,0,0]
+            kty = [0,0,0,0]
+            for i in range(0,len(l)):#跑每個資料點的index比較方便
+                kn[lSets[i]]+=1
+                ktx[lSets[i]]+=l[i][0]
+                kty[lSets[i]]+=l[i][1]
+            for i in range(0,4):
+                if(kn[i]>0):
+                    kPoints[i] = [ktx[i]/kn[i],kty[i]/kn[i]]
 
-    #主循環
-    for loop in range(10):
-        #每個點分給最近的
-        pCount = 0#點的index，0~len(l)
-        for p in l:
-            kCount = 0#群集的index，0~3
-            minDis = 999999#最小距離
-            for k in kPoints:
-                if abs(p-k)<= minDis:#如果這個點對某個中心最近
-                    lSets[pCount] = kCount#將此點設為該群集
-                    minDis = abs(p-k)
-                kCount+=1
-            pCount+=1
+            #顯示大小 群集
+            #print("---")
+            for i in range(0,len(kPoints)):
+                print("中心："+str(kPoints[i]))
+            for i in range(0,len(l)):
+                print("數值："+str(l[i][0])+","+str(l[i][1]),"群集："+str(lSets[i]))
+            print("---")
+        index = 0
+        #畫畫
+        for s in states[1:]:
+            #標記數值
+            cv2.putText(nCoinFrame, str(s[4]), (s[0], s[1]), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 255), 1, cv2.LINE_AA)
+            #畫方框
+            if lSets[index]==0:
+                cv2.rectangle(nCoinFrame,(s[0],s[1]),(s[2]+s[0],s[3]+s[1]),(0,0,255),2)
+            elif lSets[index]==1:
+                cv2.rectangle(nCoinFrame,(s[0],s[1]),(s[2]+s[0],s[3]+s[1]),(0,0,127),2)
+            elif lSets[index]==2:
+                cv2.rectangle(nCoinFrame,(s[0],s[1]),(s[2]+s[0],s[3]+s[1]),(0,255,255),2)
+            elif lSets[index]==3:
+                cv2.rectangle(nCoinFrame,(s[0],s[1]),(s[2]+s[0],s[3]+s[1]),(0,255,0),2)
+            index += 1
+    else:
+        for s in states[1:]:
+            #標記數值
+            #cv2.putText(nCoinFrame, str(s[4]), (s[0], s[1]), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 255), 1, cv2.LINE_AA)
+            #畫方框
+            if s[4]>=1900:
+                cv2.rectangle(nCoinFrame,(s[0],s[1]),(s[2]+s[0],s[3]+s[1]),(0,255,0),2)
+            elif s[4]>=1100:
+                cv2.rectangle(nCoinFrame,(s[0],s[1]),(s[2]+s[0],s[3]+s[1]),(0,255,255),2)
+            elif s[4]>=900:
+                cv2.rectangle(nCoinFrame,(s[0],s[1]),(s[2]+s[0],s[3]+s[1]),(80,127,255),2)
+            elif s[4]>=0:
+                cv2.rectangle(nCoinFrame,(s[0],s[1]),(s[2]+s[0],s[3]+s[1]),(0,0,255),2)
 
-        #重新計算每個群的中心(kPoints)
-        #各群數量與total
-        kn = [0,0,0,0]
-        kt = [0,0,0,0]
-        for i in range(0,len(l)):#跑每個資料點的index比較方便
-            kn[lSets[i]]+=1
-            kt[lSets[i]]+=l[i]
-        for i in range(0,4):
-            if(kn[i]>0):
-                kPoints[i] = kt[i]/kn[i]
-        #顯示大小 群集
-        #print("---")
-        for i in range(0,len(kPoints)):
-            print("中心："+str(kPoints[i]))
-        for i in range(0,len(l)):
-            print("數值："+str(l[i]),"群集："+str(lSets[i]))
-        print("---")
-
-    for s in states[1:]:
-        cv2.putText(nCoinFrame, str(s[4]), (s[0], s[1]), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 255), 1, cv2.LINE_AA)
     cv2.imshow("e",eCoinFrame)
     #顯示
     cv2.imshow("",np.hstack([nCoinFrame]))
