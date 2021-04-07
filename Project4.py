88aee59853a5489f0b7bbd5171cd7015f7c6f19e
pl30987 <55215480+pl30987@users.noreply.github.com>
2021-04-08 07:53:39 +0800

    testSize 完成

--- Project4.py
+++ Project4.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 #
 # svm參數參考https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
 #
-# kernel: {‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’}, default=’rbf’
+# kernel: {‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’}, default=’rbf’
 # C     : float, default=1.0
 # gamma : {‘scale’, ‘auto’} or float, default=’scale’
 #
@@ -19,17 +19,46 @@ def foo(in_x, in_y, in_kernel, in_c, in_gamma, in_sizeTest):
     svmModel = svm.SVC(kernel=in_kernel, C=in_c, gamma=in_gamma)
 
     svmModel.fit(x_train, y_train)
-    #train資料集準確率,test資料及準確率
-    return [svmModel.score(x_train, y_train), svmModel.score(x_test, y_test)]
+    #test資料及準確率
+    return svmModel.score(x_test, y_test)
 
 
 #
 # main function
 #
 def main():
-    #
-    # wine = datasets.load_wine()
-    # x=foo(wine.data, wine.target, 'linear', 1, 'auto', 0.8)
+    kernelList = ['linear', 'poly', 'rbf', 'sigmoid']
+    scoreList = []
+
+    wine = datasets.load_wine()
+    #example x=foo(wine.data, wine.target, 'linear', 1, 'auto', 0.8)
+
+
+    #sizeTest
+    plt.figure(1, (10, 6))
+    for kernel in kernelList:
+        scoreList = []
+        for size in np.linspace(0.05, 0.95, 100):
+            scoreList.append(foo(wine.data, wine.target, kernel, 1, 'auto', size))
+        plt.plot(np.linspace(0.05, 0.95, 100), scoreList, label=kernel)
+
+    plt.legend()
+    plt.xlabel('test_size')
+    plt.ylabel('score')
+    plt.title("sizeTest")
+
+
+
+    #example
+    plt.figure(2)
+
+    plt.legend()
+    plt.xlabel('test_size')
+    plt.ylabel('score')
+    plt.title("sizeTest")
+
+
+    plt.show()
 
 
 main()
