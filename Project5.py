6f8988ab39a4b4c1f56a8dff406adce8122ea684
pl30987 <55215480+pl30987@users.noreply.github.com>
2021-04-08 12:34:55 +0800

    新增 imageToHog

--- Project5.py
+++ Project5.py
@@ -1,22 +1,51 @@
+from matplotlib.axes import Axes
 import matplotlib.pyplot as plt
 import numpy as np
 
 from sklearn import datasets
-from sklearn import metrics
-from sklearn import svm
 from sklearn.model_selection import train_test_split
+from sklearn import svm
+
+from skimage.feature import hog
+from skimage import data, exposure
 
 
+def example():
+	image = data.astronaut()
+
+	fd, hog_image = hog(image, orientations=9, pixels_per_cell=(8,8),
+		cells_per_block=(3,3), visualize=True, multichannel=True)
+
+	fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+
+	ax1.axis('off')
+	ax1.imshow(image, cmap=plt.cm.gray)
+	ax1.set_title('input image')
+
+	#resize
+	hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+
+	ax2.axis('off')
+	ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+	ax2.set_title('hog image')
+
+	plt.show()
 
 
+def imageToHog(imageList):
+	resultArray = np.array()
+	for img in imageList:
+		fd, hog_image = hog(img, orientations=9, pixels_per_cell=(8,8), cells_per_block=(3,3),
+		visualize=False, multichannel=True)
+		np.append(resultArray, [fd], axis=0)
+	return resultArray
 
-def doge():
-	print('woof')
+
 
 
 
 #main
 def main():
-	doge()
+	example()
 
 main()
