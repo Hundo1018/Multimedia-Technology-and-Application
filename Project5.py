027a3ba0d0dc166661aabeac3518442c70d6934b
pl30987 <55215480+pl30987@users.noreply.github.com>
2021-04-08 11:23:33 +0800

    新增 project5.py

---
+++ Project5.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn import datasets
+from sklearn import metrics
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+
+
+
+
+
+def doge():
+	print('woof')
+
+
+
+#main
+def main():
+	doge()
+
+main()
