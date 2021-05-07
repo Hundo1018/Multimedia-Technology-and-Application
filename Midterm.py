bbe8fe8f3de9f9f48faf96d58835b691fca1ed7b
pl30987 <55215480+pl30987@users.noreply.github.com>
2021-05-07 23:38:59 +0800

    修好了

--- Midterm.py
+++ Midterm.py
@@ -45,12 +45,16 @@ def resultRockPaperScissors(user, bot):
 
 
 # 手部判斷 需要SVM 回傳預測結果
-def reconHand(img, svm):
+def reconHand(img, svm, voc):
 	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	img = cv2.resize(img, (500, 500))
 	sift = cv2.SIFT_create()
 	kp, des = sift.detectAndCompute(img, None)
-	result = svm.predict(des.flatten())
+	im_feature = np.zeros((1, 40), "float32")
+	words, distance = vq(des, voc)
+	for w in words:
+		im_feature[w] += 1
+	result = svm.predict(im_feature)
 	return result
 
 
@@ -99,13 +103,10 @@ def combineImg(imgUser, imgBot, status):
 	return resultImg
 
 
-
-
-
 # 0: 剪刀
 # 1: 石頭
 # 2: 布
-def RockPaperScissors():
+def RockPaperScissors(voc):
 	# load bot imagine for show
 	imgBotRock = cv2.imread("botRock.jpg")
 	imgBotPaper = cv2.imread("botPaper.jpg")
@@ -121,7 +122,7 @@ def RockPaperScissors():
 		print("影像擷取中... 請按Q確定擷取。")
 		userImg = Camara()						# 從攝影機載入影像
 		# userImg = cv2.imread("user.jpg")		# load user hand input imagine
-		userHand = reconHand(userImg, svm)		# 玩家的手 整數
+		userHand = reconHand(userImg, svm, voc)		# 玩家的手 整數
 		if userHand == 0:
 			print("玩家手勢錯誤! 請在試一次")
 		else:
@@ -141,65 +142,6 @@ def RockPaperScissors():
 
 	gameResultImg = combineImg(userImg, botImg, gameResult)
 	cv2.imshow("遊戲結果~", gameResultImg)
-
-	train_path = "dataset/train"
-	training_names = os.listdir(train_path)
-
-	image_paths = []
-	image_classes = []
-	class_id = 0
-	for training_name in training_names:
-		dir = os.path.join(train_path, training_name)
-		class_path = list(paths.list_images(dir))
-		image_paths += class_path
-		image_classes += [class_id] * len(class_path)
-		class_id += 1
-
-	sift = cv2.SIFT_create()
-
-	des_list = []
-
-	for image_path in image_paths:
-		im = cv2.imread(image_path)
-		im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-		im = cv2.resize(im, (500, 500))
-		kpts = sift.detect(im)
-		kpts, des = sift.compute(im, kpts)
-		des_list.append((image_path, des))
-		print("Image file path : ", image_path)
-
-	descriptors = des_list[0][1]
-	for image_path, descriptor in des_list[1:]:
-		descriptors = np.vstack((descriptors, descriptor))
-
-	k = 30
-	voc, variance = kmeans(descriptors, k, 5)
-
-	im_features = np.zeros((len(image_paths), k), "float32")
-	for i in range(len(image_paths)):
-		words, distance = vq(des_list[i][1], voc)
-		for w in words:
-			im_features[i][w] += 1
-
-	x = im_features
-	y = np.array(image_classes)
-	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-
-	clf = svm.SVC()
-	clf.fit(x_train,y_train)
-
-	print("predict")
-	print(clf.predict(x_train))
-	print(clf.predict(x_test))
-
-	print("Accuracy:")
-	print(clf.score(x_train,y_train))
-	print(clf.score(x_test,y_test))
-
-	stdslr = StandardScaler().fit(im_features)
-	im_features = stdslr.transform(im_features)
-
-	return clf
 
 #svm訓練區
 def svmTeacher():
@@ -223,29 +165,30 @@ def svmTeacher():
 	for image_path in image_paths:
 		im = cv2.imread(image_path)
 		im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-		im = cv2.resize(im, (500, 500))
-		kpts = sift.detect(im)
-		kpts, des = sift.compute(im, kpts)
+		im = cv2.resize(im, (300, 200))
+		kpts, des = sift.detectAndCompute(im, None)
+		if not np.any(des):
+			des = np.zeros((1, 128), dtype="float32")
 		des_list.append((image_path, des))
 		print("Image file path : ", image_path)
 
 	descriptors = des_list[0][1]
 	for image_path, descriptor in des_list[1:]:
-		if descriptor is not None:
-			descriptors = np.vstack((descriptors, descriptor))
+		descriptors = np.vstack((descriptors, descriptor))
 
 	k = 40
 	voc, variance = kmeans(descriptors, k, 20)
 
 	im_features = np.zeros((len(image_paths), k), "float32")
 	for i in range(len(image_paths)):
-		if des_list[i][1] is None:
-			continue
+
 		words, distance = vq(des_list[i][1], voc)
 		for w in words:
 			im_features[i][w] += 1
+
 	stdslr = StandardScaler().fit(im_features)
 	im_features = stdslr.transform(im_features)
+
 	x = im_features
 	y = np.array(image_classes)
 	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
@@ -263,15 +206,16 @@ def svmTeacher():
 
 
 
-	return clf
+	return clf, voc
 
 def main():
 	# 訓練
 	#clf = joblib.load("hand_svm.pkl")
-	clf = svmTeacher()
+	clf, voc = svmTeacher()
 	joblib.dump(clf,"hand_svm.pkl")
 	# 剪刀石頭布程式，不包含訓練
-	RockPaperScissors()
+	while 1:
+		RockPaperScissors(voc)
 	pass
 
 main()
